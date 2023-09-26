2565f72631ab7be08bf830ed082f6beae6efd461
Mohamed <mohamed10.m@outlook.com>
2023-09-25 17:31:36 -0700

    lists

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,37 +1,29 @@
-#include "lists.h"
-
+#include "list"
 
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *p2;
-	listint_t *prev;
+       	listint_t *slow = head;
+       	listint_t *fast = head;
 
-	p2 = head;
-	prev = head;
-	while (head && p2 && p2->next)
+       	if (!head)
+	       	return (NULL);
+
+       	while (slow && fast && fast->next)
 	{
-		head = head->next;
-		p2 = p2->next->next;
 
-		if (head == p2)
-		{
-			head = prev;
-			prev =  p2;
-			while (1)
-			{
-				p2 = prev;
-				while (p2->next != head && p2->next != prev)
-				{
-					p2 = p2->next;
-				}
-				if (p2->next == head)
-					break;
+		fast = fast->next->next;
+	       	slow = slow->next;
+	       	if (fast == slow)
+	       	{
+		       	slow = head;
+		       	while (slow != fast)
+		       	{
+			       	slow = slow->next;
+			       	fast = fast->next;
+		       	}
+		       	return (fast);
+	       	}
+       	}
 
-				head = head->next;
-			}
-			return (p2->next);
-		}
-	}
-
-	return (NULL);
+       	return (NULL);
 }
