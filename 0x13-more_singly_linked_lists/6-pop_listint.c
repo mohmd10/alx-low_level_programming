ce5facfe255730ce19eacbd9d845f38332cda207
Mohamed <mohamed10.m@outlook.com>
2023-09-26 17:36:06 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -13,16 +13,18 @@
 
 int pop_listint(listint_t **head)
 {
-	listint_t *popped;
-	int content;
+	listint_t *temp_variable; /* keep track of current head */
+	int data = 0;
 
-	if (*head == NULL)
+    /* if there is no list */
+	if (head == NULL || *head == NULL)
 		return (0);
-
-	popped = *head;
-	content = popped->n;
-	free(popped);
-
+	temp_variable = *head; /* stores the pointer to temp_variable head */
+    /* access the field n of temp_var and assign it to data */
+	data = temp_variable->n;
+    /* make the value at head be the value of head at next */
 	*head = (*head)->next;
-	return (content);
+    /* free the temp var */
+	free(temp_variable);
+	return (data);
 }
